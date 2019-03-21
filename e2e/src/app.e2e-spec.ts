f445177f55f500a718af680b1b9081ce94d6a165
Camilo_Tabares <camilo.tabares.84@gmail.com>
2019-03-20 23:30:53 -0500

    changin name of code challenge

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to codeChallenge!');
+    expect(page.getTitleText()).toEqual('Welcome to faces_challenge!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'codeChallenge'`, () => {
+  it(`should have as title 'faces_challenge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('codeChallenge');
+    expect(app.title).toEqual('faces_challenge');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to codeChallenge!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to faces_challenge!');
   });
 });
